5c8b731a641fa3fd5d837fc388199fe94d844740
ASUS <ASUS@Wellcome>
2016-11-21 11:50:12 +0700

    gitignore

---
+++ parent/web/src/main/java/org/cntt/web/controller/homeController.java
@@ -0,0 +1,14 @@
+package org.cntt.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class homeController {
+
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public String home(){
+		return "new";
+	}
+}
