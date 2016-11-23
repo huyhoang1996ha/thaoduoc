2fa30c35306d1f648a06b561cb5babe3686ab77d
ASUS <ASUS@Wellcome>
2016-11-24 00:19:17 +0700

    fix service

--- parent/product/src/main/java/org/cntt/product/service/ProductServiceImpl.java
+++ parent/product/src/main/java/org/cntt/product/service/ProductServiceImpl.java
@@ -3,12 +3,12 @@ package org.cntt.product.service;
 import org.cntt.model.product.Product;
 import org.cntt.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
+@Service
 public class ProductServiceImpl implements ProductService{
 	@Autowired
 	ProductRepository productRepository;
 	
-	@Override
 	public Product findOne(int id) {
 		// TODO Auto-generated method stub
 		Product product=productRepository.findOne(id);
